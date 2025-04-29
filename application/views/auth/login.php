a4f97c3bd69468e6074a20f2273741b9e422d534
trionoaja <trionoaja@gmail.com>
2025-04-29 21:16:18 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -32,7 +32,7 @@
         <?php endif;?>
       <form action="<?= site_url('auth/process_login')?>" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" name="username" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -40,7 +40,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
